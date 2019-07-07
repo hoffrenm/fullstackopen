58daeecfd952129f04ea83d8edd48611b18c9e41
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-07-08 00:51:17 +0300

    Finish exercises 2.1-2.5

---
+++ osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((a, b) => a + b.exercises, 0);
+
+  return <b>Total of {total} exercises</b>;
+};
+
+export default Course;
