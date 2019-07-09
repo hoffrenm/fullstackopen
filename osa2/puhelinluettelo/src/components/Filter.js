c9704793ccba5f862cfc4aa1ea947f585a655d92
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-07-09 13:49:59 +0300

    2.9: Fix formatting

--- osa2/puhelinluettelo/src/components/Filter.js
+++ osa2/puhelinluettelo/src/components/Filter.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Filter = ({ handleChange, text }) => {
-    return (
-        <div>
-            Filter list: <input value={text} onChange={handleChange} />
-        </div>
-    )
-}
+  return (
+    <div>
+      Filter list: <input value={text} onChange={handleChange} />
+    </div>
+  );
+};
 
 export default Filter;
