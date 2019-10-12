7d17964d6dce2abd0158eb16b3b40f0d7342e946
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-10-12 23:22:32 +0300

    add middleware

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,8 +4,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const logger = require("./utils/logger");
 
@@ -20,11 +22,19 @@ mongoose
     logger.error("error connection to MongoDB:", error.message);
   });
 
+
 app.use(cors());
 app.use(express.static("build"));
 app.use(bodyParser.json());
 
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor);
+
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app;
