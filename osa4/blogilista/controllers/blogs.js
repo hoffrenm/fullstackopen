c1ba9896b1d9adfd357b2a77f8d36bb7dfc364fb
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-10-12 13:59:50 +0300

    finish 4.8-4.14, refactor to use async

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,18 +1,39 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => response.json(blogs))
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+
+  response.json(blogs.map(blog => blog.toJSON()));
 });
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.post("/", async (request, response) => {
+  if (request.body.title === undefined || request.body.url === undefined) {
+    return response.status(400).end();
+  }
 
-  blog
-    .save()
-    .then(result => response.status(201).json(result))
+  const blog = new Blog({ ...request.body, likes: request.body.likes || 0 });
+
+  try {
+    const result = await blog.save();
+    response.status(201).json(result.toJSON());
+  } catch (exception) {}
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (exception) {}
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const blog = { ...request.body };
+
+  try {
+    const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+    response.status(204).json(result.toJSON());
+  } catch (exception) {}
 });
 
 module.exports = blogsRouter;
