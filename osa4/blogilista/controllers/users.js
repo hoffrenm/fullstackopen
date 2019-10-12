d1c515bbaa2b10e8a4271cc074bc61b2b8b5cab4
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-10-12 20:00:21 +0300

    4.15-4.16, functionality and tests to user creation

---
+++ osa4/blogilista/controllers/users.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response, next) => {
+    const users = await User.find({}).populate("blogs", { title: 1, url: 1, author: 1 })
+    response.json(users.map(user => user.toJSON()))
+});
+
+usersRouter.post("/", async (request, response, next) => {
+  try {
+    const body = request.body;
+
+    if (body.username.length > 2 && body.password.length > 2) {
+        const usernameInUse = await User.findOne({ username: body.username });
+
+        if (usernameInUse) {
+            return response.status(400).json({ error: "username is already in use"})
+        }
+    } else {
+        return response.status(400).json({ error: "username and/or password should be atleast 3 digits long"})
+    }
+    
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash
+    });
+
+    const savedUser = await user.save();
+
+    response.json(savedUser.toJSON());
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+module.exports = usersRouter;
---
+++ osa4/blogilista/tests/user_api.test.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const User = require("../models/user");
+const helper = require("./test_helper");
+const api = supertest(app);
+
+describe("when there is initially one user at db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+    const user = new User({
+      username: "root",
+      name: "Ro Juut",
+      password: "sekret"
+    });
+    await user.save();
+  });
+
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    await api
+      .post("/api/users")
+      .send(helper.newuser)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map(u => u.username);
+    expect(usernames).toContain(helper.newuser.username);
+  });
+
+  test("dublicate username is rejected", async () => {
+      const existingUser = {
+        username: "root",
+        name: "Ro Juut",
+        password: "sekret"
+      };
+
+      await api
+        .post("/api/users")
+        .send(existingUser)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
+    });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
