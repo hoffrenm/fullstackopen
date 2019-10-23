99575f0772d758a73ff9102ceadda463cef75dc8
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-10-23 17:24:28 +0300

    5.7-5.8, add like and delete functionality

--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,35 +1,43 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const baseUrl = "/api/blogs";
 
-let token = null
+let token = null;
 
 const setToken = newToken => {
-  token = `bearer ${newToken}`
-}
+  token = `bearer ${newToken}`;
+};
 
 const removeToken = () => {
   token = null;
-}
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
 const create = async newBlog => {
   const config = {
-    headers: { Authorization: token },
-  }
+    headers: { Authorization: token }
+  };
 
-  const response = await axios.post(baseUrl, newBlog, config)
-  
-  return response.data
-}
+  const response = await axios.post(baseUrl, newBlog, config);
 
-const update = (id, newBlog) => {
-  const request = axios.put(`${baseUrl}/${id}`, newBlog)
-  
-  return request.then(response => response.data)
-}
+  return response.data;
+};
 
-export default { getAll, create, update, setToken, removeToken }
+const update = async (id, newBlog) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newBlog);
+
+  return response.data;
+};
+
+const remove = async id => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  await axios.delete(`${baseUrl}/${id}`, config);
+};
+
+export default { getAll, create, update, remove, setToken, removeToken };
